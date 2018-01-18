8193025fce934f07e15c276e3b87791619e059db
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2018-01-18 14:34:45 +0800

    Initial commit

---
+++ JS/login.js
@@ -0,0 +1,21 @@
+function login(){
+  chrome.storage.local.get(['ArrName','ArrValue','btnName'],function(valArray){
+    for(var start = 1;start<=valArray.ArrName.length;start++){
+      document.getElementsByName(valArray.ArrName[start-1])[0].value=valArray.ArrValue[start-1];
+    }
+    document.getElementsByName(valArray.btnName)[0].click();
+  });
+};
+login();
+chrome.extension.onRequest.addListener(
+  function(request, sender, sendResponse){
+    chrome.storage.local.get(['btnName'],function(valArray){
+      if(document.getElementsByName(valArray.btnName)[0]==undefined){
+        sendResponse({result: "failed"});
+      }else{
+        sendResponse({result: "success"});
+        login();
+      }
+    });
+  }
+);
