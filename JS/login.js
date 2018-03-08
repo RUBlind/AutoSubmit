053400e3bfd011389c6775c5167c989275923c8d
NB-Dragon <34410109+NB-Dragon@users.noreply.github.com>
2018-03-08 14:04:26 +0800

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,7 +1,7 @@
 function login(){
   chrome.storage.local.get(['ArrName','ArrValue','btnName'],function(valArray){
     for(var start = 1;start<=valArray.ArrName.length;start++){
-      if(valArray.ArrName[start-1])[0].value=="")continue;
+      if(valArray.ArrName[start-1]=="")continue;
       document.getElementsByName(valArray.ArrName[start-1])[0].value=valArray.ArrValue[start-1];
     }
     document.getElementsByName(valArray.btnName)[0].click();
