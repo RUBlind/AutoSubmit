387bea92ad2e31a4d49a56186298c6cb0a1c557f
yanzu-zz <2273359360@qq.com>
2018-11-10 14:49:55 +0800

    优化了下代码

--- Resource/JS/background.js
+++ Resource/JS/background.js
@@ -1,38 +1,65 @@
 ﻿chrome.contextMenus.create({
-    type: 'normal',
-    title: '开始网页监控',
-    contexts: ['page'],
-    onclick: function () {
-        autoRefresh();
-    },
-});
+  type: 'normal',
+  title: '开始网页监控',
+  contexts: ['page'],
+  onclick: function () {
+    autoRefresh()
+  },
+})
 chrome.contextMenus.create({
-    type: 'normal',
-    title: '停止网页监控',
-    contexts: ['page'],
-    onclick: function () {
-        stopRefresh();
-    },
-});
+  type: 'normal',
+  title: '停止网页监控',
+  contexts: ['page'],
+  onclick: function () {
+    stopRefresh()
+  },
+})
 
-var f_timer;
+var f_timer
 
 function autoRefresh() {
-    clearInterval(f_timer);
-    f_timer = setInterval(function () {
-        chrome.tabs.getSelected(null, function (tab) {
-            if (tab.id === -1) return;
-            chrome.tabs.sendRequest(tab.id, {status: "true"}, function (response) {
-                if (response !== undefined && response.result === "failed") {
-                    chrome.tabs.reload();
-                } else {
-                    stopRefresh();
-                }
-            });
-        });
-    }, 200);
+  debounce(function () {
+    chrome.tabs.getSelected(null, function (tab) {
+      if (tab.id === -1) return
+      
+      chrome.tabs.sendRequest(tab.id, { status: "true" }, function (response) {
+        if (response !== undefined && response.result === "failed") {
+          chrome.tabs.reload()
+        } else {
+          stopRefresh()
+        }
+      })
+    })
+  }, 200)
+
+  // clearInterval(f_timer)
+  // f_timer = setInterval(function () {
+  //   chrome.tabs.getSelected(null, function (tab) {
+  //     if (tab.id === -1) return
+  //     chrome.tabs.sendRequest(tab.id, { status: "true" }, function (response) {
+  //       if (response !== undefined && response.result === "failed") {
+  //         chrome.tabs.reload()
+  //       } else {
+  //         stopRefresh()
+  //       }
+  //     })
+  //   })
+  // }, 200)
 }
 
 function stopRefresh() {
-    clearInterval(f_timer);
+  clearInterval(f_timer)
+}
+
+// 函数节流
+function debounce(func, delay) {
+  return function (...args) {
+    if (f_timer) {
+      clearTimeout(f_timer)
+    }
+
+    f_timer = setTimeout(() => {
+      func.apply(this, args)
+    }, delay) // 如果想传入 秒 做为单位，则让 delay * 1000 即可
+  }
 }
--- Resource/JS/optionsPage.js
+++ Resource/JS/optionsPage.js
@@ -1,39 +1,45 @@
-﻿function loadOption() {
-    chrome.storage.local.get(['ArrName', 'ArrValue', 'btnName'], function (valArray) {
-        if (JSON.stringify(valArray) === "{}") return;
-        let max = document.querySelectorAll('tbody > tr > td > input').length;
-        for (let start = 1; start <= Math.floor(max / 2); start++) {
-            document.getElementById("name_" + start).value = valArray.ArrName[start - 1];
-            document.getElementById("value_" + start).value = valArray.ArrValue[start - 1];
-        }
-        document.getElementById('value_submit').value = valArray.btnName;
-        console.log("加载成功");
-    });
+﻿function initPageContentAndListener() {
+  let saveButton = document.querySelector('#save_btn')
+  let clearButton = document.querySelector('#clear_btn')
+
+  saveButton.addEventListener('click', function () {
+    let btnName = document.querySelector('#value_submit').value
+    let max = document.querySelectorAll('tbody > tr > td > input').length
+    let ArrName = [], ArrValue = []
+
+    for (let start = 1; start <= Math.floor(max / 2); start++) {
+      ArrName[start - 1] = document.querySelector('#name_' + start).value
+      ArrValue[start - 1] = document.querySelector('#value_' + start).value
+    }
+
+    chrome.storage.local.set({'ArrName': ArrName, 'ArrValue': ArrValue, 'btnName': btnName }, function () {
+      console.log(ArrName, ArrValue, btnName)
+      alert('配置信息更新成功')
+    })
+  })
+
+  clearButton.addEventListener('click', function () {
+    Array.from(document.querySelectorAll('input')).forEach(item => item.value = '')
+    chrome.storage.local.clear(function () {
+      alert('配置信息清除成功')
+    })
+  })
+
+  loadOption()
 }
 
-function initPageContentAndListener() {
-    let saveButton = document.getElementById("save_btn");
-    let clearButton = document.getElementById("clear_btn");
-    saveButton.addEventListener("click", function () {
-        let btnName = document.getElementById('value_submit').value;
-        let max = document.querySelectorAll('tbody > tr > td > input').length;
-        let ArrName = [], ArrValue = [];
-        for (let start = 1; start <= Math.floor(max / 2); start++) {
-            ArrName[start - 1] = document.getElementById("name_" + start).value;
-            ArrValue[start - 1] = document.getElementById("value_" + start).value;
-        }
-        chrome.storage.local.set({'ArrName': ArrName, 'ArrValue': ArrValue, 'btnName': btnName}, function () {
-            console.log(ArrName, ArrValue, btnName);
-            alert("配置信息更新成功");
-        });
-    });
-    clearButton.addEventListener("click", function () {
-        Array.from(document.getElementsByTagName('input')).forEach(item => item.value = "");
-        chrome.storage.local.clear(function () {
-            alert("配置信息清除成功");
-        });
-    });
-    loadOption();
+function loadOption() {
+  chrome.storage.local.get(['ArrName', 'ArrValue', 'btnName'], function (valArray) {
+    if (JSON.stringify(valArray) === '{}') return
+    let max = document.querySelectorAll('tbody > tr > td > input').length
+    for (let start = 1; start <= Math.floor(max / 2); start++) {
+      document.querySelector('#name_' + start).value = valArray.ArrName[start - 1]
+      document.querySelector('#value_' + start).value = valArray.ArrValue[start - 1]
+    }
+
+    document.querySelector('#value_submit').value = valArray.btnName
+    console.log('加载成功')
+  })
 }
 
-initPageContentAndListener();
+initPageContentAndListener()
