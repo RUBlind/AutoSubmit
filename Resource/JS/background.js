88f8e57650a01026f493eb5d732251693ec27c4f
NB-Dragon <826336127@qq.com>
2018-11-10 18:58:08 +0800

    1.每行代码后面保留分号提示已结束 2.每行代码的缩进以四个空格为单位 3.鉴于TimeOut计时器不利于长时间的监控，要改回Interval

--- Resource/JS/background.js
+++ Resource/JS/background.js
@@ -1,65 +1,38 @@
 ﻿chrome.contextMenus.create({
-  type: 'normal',
-  title: '开始网页监控',
-  contexts: ['page'],
-  onclick: function () {
-    autoRefresh()
-  },
-})
+    type: 'normal',
+    title: '开始网页监控',
+    contexts: ['page'],
+    onclick: function () {
+        autoRefresh();
+    },
+});
 chrome.contextMenus.create({
-  type: 'normal',
-  title: '停止网页监控',
-  contexts: ['page'],
-  onclick: function () {
-    stopRefresh()
-  },
-})
+    type: 'normal',
+    title: '停止网页监控',
+    contexts: ['page'],
+    onclick: function () {
+        stopRefresh();
+    },
+});
 
-var f_timer
+let f_timer;
 
 function autoRefresh() {
-  debounce(function () {
-    chrome.tabs.getSelected(null, function (tab) {
-      if (tab.id === -1) return
-      
-      chrome.tabs.sendRequest(tab.id, { status: "true" }, function (response) {
-        if (response !== undefined && response.result === "failed") {
-          chrome.tabs.reload()
-        } else {
-          stopRefresh()
-        }
-      })
-    })
-  }, 200)
-
-  // clearInterval(f_timer)
-  // f_timer = setInterval(function () {
-  //   chrome.tabs.getSelected(null, function (tab) {
-  //     if (tab.id === -1) return
-  //     chrome.tabs.sendRequest(tab.id, { status: "true" }, function (response) {
-  //       if (response !== undefined && response.result === "failed") {
-  //         chrome.tabs.reload()
-  //       } else {
-  //         stopRefresh()
-  //       }
-  //     })
-  //   })
-  // }, 200)
+    clearInterval(f_timer);
+    f_timer = setInterval(function () {
+        chrome.tabs.getSelected(null, function (tab) {
+            if (tab.id === -1) return;
+            chrome.tabs.sendRequest(tab.id, {status: "true"}, function (response) {
+                if (response !== undefined && response.result === "failed") {
+                    chrome.tabs.reload();
+                } else {
+                    stopRefresh();
+                }
+            });
+        });
+    }, 200);
 }
 
 function stopRefresh() {
-  clearInterval(f_timer)
-}
-
-// 函数节流
-function debounce(func, delay) {
-  return function (...args) {
-    if (f_timer) {
-      clearTimeout(f_timer)
-    }
-
-    f_timer = setTimeout(() => {
-      func.apply(this, args)
-    }, delay) // 如果想传入 秒 做为单位，则让 delay * 1000 即可
-  }
+    clearInterval(f_timer);
 }
--- Resource/JS/login.js
+++ Resource/JS/login.js
@@ -1,31 +1,27 @@
 function login() {
-  chrome.storage.local.get(['ArrName', 'ArrValue', 'btnName'], function (valArray) {
-    for (let start = 1; start <= valArray.ArrName.length; start++) {
-      if (valArray.ArrName[start - 1] === "") continue
-      let inputElement = document.getElementsByName(valArray.ArrName[start - 1])[0]
-      if (inputElement === undefined) continue
-      inputElement.value = valArray.ArrValue[start - 1]
-    }
-    
-    let submitElement = document.getElementsByName(valArray.btnName)[0]
-    if (submitElement === undefined) return
-    
-    submitElement.click()
-  })
+    chrome.storage.local.get(['ArrName', 'ArrValue', 'btnName'], function (valArray) {
+        for (let start = 1; start <= valArray.ArrName.length; start++) {
+            if (valArray.ArrName[start - 1] === "") continue;
+            let inputElement = document.getElementsByName(valArray.ArrName[start - 1])[0];
+            if (inputElement === undefined) continue;
+            inputElement.value = valArray.ArrValue[start - 1];
+        }
+        let submitElement = document.getElementsByName(valArray.btnName)[0];
+        if (submitElement === undefined) return;
+        submitElement.click();
+    });
 }
 
-// execute login
-login()
-
+login();
 chrome.extension.onRequest.addListener(
-  function (request, sender, sendResponse) {
-    chrome.storage.local.get(['btnName'], function (valArray) {
-      if (document.getElementsByName(valArray.btnName)[0] === undefined) {
-        sendResponse({ result: "failed" })
-      } else {
-        sendResponse({ result: "success" })
-        login()
-      }
-    })
-  }
-)
+    function (request, sender, sendResponse) {
+        chrome.storage.local.get(['btnName'], function (valArray) {
+            if (document.getElementsByName(valArray.btnName)[0] === undefined) {
+                sendResponse({result: "failed"});
+            } else {
+                sendResponse({result: "success"});
+                login();
+            }
+        });
+    }
+);
